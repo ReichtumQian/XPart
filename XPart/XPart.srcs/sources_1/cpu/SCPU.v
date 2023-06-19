e099c1b4ec7dcb02379ccd802ae966a0827dc73b
reichtum <qyx18267223529@outlook.com>
2023-06-19 16:22:36 +0800

    Fix a warning.

--- XPart/XPart.srcs/sources_1/cpu/SCPU.v
+++ XPart/XPart.srcs/sources_1/cpu/SCPU.v
@@ -807,7 +807,7 @@ module SCPU(
     wire wb_is_lw = wb_funct3 == 010;
     wire wb_is_lbu = wb_funct3 == 100;
     assign wb_memory_write_data = (wb_is_ld) ? wb_memory_data : 
-                                   (wb_is_lw) ? {{32{0}},{wb_memory_data[31:0]}} :
+                                   (wb_is_lw) ? {{32'b0},{wb_memory_data[31:0]}} :
                                    (wb_is_lbu) ? {56'b0,wb_memory_data[7:0]} :
                                    64'b0;
     MUX8T1_32 write_register_mux(
